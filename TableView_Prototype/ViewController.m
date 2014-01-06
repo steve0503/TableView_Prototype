4a98689147d9f2a7daa94f4820f7e662a2bb95af
steve0503 <leehr76@gmail.com>
2014-01-06 16:09:19 +0900

    TableView_168
    Done

--- TableView_Prototype/ViewController.m
+++ TableView_Prototype/ViewController.m
@@ -8,16 +8,41 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define CELL_ID @"CELL_ID"
+
+@interface ViewController ()<UITableViewDataSource>{
+    
+    NSArray *data;
+    
+}
+
+
 
 @end
 
 @implementation ViewController
 
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+    
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return [data count];
+    
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    data = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i",@"j",@"k",@"l",@"m",@"n",@"o",@"p"];
 }
 
 - (void)didReceiveMemoryWarning
